b6ffeb1355b477b639cc508022bbade00326665c
Ivan Cesar <icesar@tvz.hr>
2020-03-16 22:14:29 +0100

    Rjesenja vjezbe 2

---
+++ src/Vjezba/Vjezba.Model/Fakultet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vjezba.Model
+{
+    public class Fakultet
+    {
+        public List<Osoba> Osobe { get; set; } = new List<Osoba>();
+
+        public int KolikoProfesora() => Osobe.OfType<Profesor>().Count();
+        public int KolikoStudenata() => Osobe.OfType<Student>().Count();
+
+        public Student DohvatiStudenta(string jmbag)
+            => Osobe.OfType<Student>().FirstOrDefault(s => s.JMBAG == jmbag);
+
+        public IEnumerable<Profesor> DohvatiProfesore()
+            => Osobe.OfType<Profesor>().OrderBy(p => p.DatumIzbora);
+    }
+}
---
+++ src/Vjezba/Vjezba.Model/Profesor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba.Model
+{
+    public class Profesor : Osoba
+    {
+        public string Odjel { get; set; }
+        public Zvanje Zvanje { get; set; }
+        public DateTime DatumIzbora { get; set; }
+
+        public int KolikoDoReizbora()
+        {
+            var next = DatumIzbora.AddYears(Zvanje == Zvanje.Asistent ? 4 : 5);
+            return (int)((next - DateTime.Now).TotalDays / 365.0);
+        }
+    }
+
+    public enum Zvanje
+    {
+        Asistent, Predavac, VisiPredavac, ProfVisokeSkole
+    }
+}
