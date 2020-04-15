5bd9f16caaee48563ff60bcc0403d595829bc5ad
Ivan Cesar <icesar@tvz.hr>
2020-04-15 10:24:33 +0200

    Rjesenja zadataka 5

---
+++ src/Vjezba/Vjezba.Web/Models/ClientModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vjezba.Web.Models
+{
+    public class ClientFilterModel
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+    }
+}
